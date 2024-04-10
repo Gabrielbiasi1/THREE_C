9ac39a66e11ade4236f6768fa5903b3804c355ae
Gabriel De Biasi <128563787+Gabrielbiasi1@users.noreply.github.com>
2024-04-10 19:29:49 -0300

    Update three_c.h

--- three_c.h
+++ three_c.h
@@ -1,22 +1,22 @@
 //arvore.h
-//Contém a implementação de uma ABB (Árvore Binária de Busca)
+//ContÃ©m a implementaÃ§Ã£o de uma ABB (Ãrvore BinÃ¡ria de Busca)
 
-//Representa um nó da ABB
+//Representa um nÃ³ da ABB
 typedef struct NoArvore
 {
-	int Valor;	//Valor armazenado neste nó da ABB	
-	struct NoArvore *Esquerda; //Endereço de memória onde o filha à esquerda deste nós está localizado
-	struct NoArvore *Direita; //Endereço de memória onde o filha à direita deste nós está localizado
+	int Valor;	//Valor armazenado neste nÃ³ da ABB	
+	struct NoArvore *Esquerda; //EndereÃ§o de memÃ³ria onde o filha Ã  esquerda deste nÃ³s estÃ¡ localizado
+	struct NoArvore *Direita; //EndereÃ§o de memÃ³ria onde o filha Ã  direita deste nÃ³s estÃ¡ localizado
 }NoArvore;
 
 //Representa uma ABB
 typedef struct Arvore
 {
-	NoArvore *Raiz; //Endereço da raiz da ABB
-	int Tamanho; //Quantidade de nós da ABB
+	NoArvore *Raiz; //EndereÃ§o da raiz da ABB
+	int Tamanho; //Quantidade de nÃ³s da ABB
 }Arvore;
 
-//Cria uma nova ABB e retorna o endereço de memória onde ela foi criada. Caso não haja memória suficiente, retorna nulo.
+//Cria uma nova ABB e retorna o endereÃ§o de memÃ³ria onde ela foi criada. Caso nÃ£o haja memÃ³ria suficiente, retorna nulo.
 Arvore* ArvoreNovo()
 {
 	Arvore *a = malloc(sizeof(Arvore));
@@ -30,8 +30,8 @@ Arvore* ArvoreNovo()
 	return a;
 }
 
-//Insere o valor informado na árvore informada
-//Retorna 1 se a inserção foi feita com sucesso, 0 caso contrário
+//Insere o valor informado na Ã¡rvore informada
+//Retorna 1 se a inserÃ§Ã£o foi feita com sucesso, 0 caso contrÃ¡rio
 int ArvoreInserir(Arvore *a, int valor)
 {
 	if(a == NULL)
@@ -69,14 +69,14 @@ int ArvoreInserir(Arvore *a, int valor)
 	return 1;
 }
 
-//Remover o nó que contem o valor informado da árvore informada
-//Retorna 1 se remoção com sucesso, 0 cc
+//Remover o nÃ³ que contem o valor informado da Ã¡rvore informada
+//Retorna 1 se remoÃ§Ã£o com sucesso, 0 cc
 int ArvoreRemover(Arvore *a, int valor)
 {
 	if(a == NULL || a->Tamanho == 0)
 		return 0;
 	
-	//Procurando o nó	
+	//Procurando o nÃ³	
 	NoArvore *atual = a->Raiz;
 	NoArvore *pai = NULL;
 	
@@ -90,14 +90,14 @@ int ArvoreRemover(Arvore *a, int valor)
 			atual = atual->Direita;
 	}
 	
-	//Se não existe, termina
+	//Se nÃ£o existe, termina
 	if(atual == NULL)
 		return 0;
 	
-	//Se existe, decidir em qual situação ele se encontra
+	//Se existe, decidir em qual situaÃ§Ã£o ele se encontra
 	if(atual->Esquerda == NULL && atual->Direita == NULL)
 	{	
-		//remoção de nó folha	
+		//remoÃ§Ã£o de nÃ³ folha	
 		if(pai == NULL)
 			a->Raiz = NULL;
 		else if(valor < pai->Valor)
@@ -107,7 +107,7 @@ int ArvoreRemover(Arvore *a, int valor)
 	}
 	else if(atual->Esquerda != NULL && atual->Direita == NULL)
 	{
-		//remoção de nó apenas com filho à esquerda
+		//remoÃ§Ã£o de nÃ³ apenas com filho Ã  esquerda
 		if(pai == NULL)
 			a->Raiz = atual->Esquerda;
 		else if(valor < pai->Valor)
@@ -117,7 +117,7 @@ int ArvoreRemover(Arvore *a, int valor)
 	}
 	else if(atual->Esquerda == NULL && atual->Direita != NULL)
 	{
-		//remoção de nó apenas com filho à direita
+		//remoÃ§Ã£o de nÃ³ apenas com filho Ã  direita
 		if(pai == NULL)
 			a->Raiz = atual->Direita;
 		else if(valor < pai->Valor)
@@ -127,7 +127,7 @@ int ArvoreRemover(Arvore *a, int valor)
 	}
 	else
 	{
-		//remoção de nó com ambos os filhos
+		//remoÃ§Ã£o de nÃ³ com ambos os filhos
 		
 		//Encontrar o substituto
 		NoArvore *substituto = atual->Esquerda;
@@ -159,6 +159,10 @@ int ArvoreRemover(Arvore *a, int valor)
 	a->Tamanho = a->Tamanho - 1;
 	return 1;
 }
+
+
+
+
 
 
 
